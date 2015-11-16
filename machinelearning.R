f78baca45c5c4b24ac4f497d3a7e1d9b917e0348
Alexandra McElhinney <lex.mcelhinney@gmail.com>
2015-11-16 13:50:59 +0000

    Base cases

--- machinelearning.R
+++ machinelearning.R
@@ -7,11 +7,25 @@ owls.training<-owls[ind==1, 1:4]#all rows set as 1 are for training, 2/3 of set
 owls.testing<-owls[ind==2, 1:4]#all rows with 2 are set for testing
 owls.trainLabel<-owls[ind==1, 5]
 owls.testLabel<-owls[ind==2,5]
-
+owls.barnowl <- c(owls$type==BarnOwl)
 
 #takes in the training set and outputs a prediction of trainLabel
-model<-function(owls.training){
+model<-function(owls.training, owls.trainLabel){
+  
+  if(length(owls.training) == 0){
+    #END 
+    print("emty training set")
+  }else if(duplicated(owls.trainLabel)){
+    print("all classes the same")
+  }
+    #end
+  else if (length(owls.trainLabel)==0){
+    print("No labels")
+    #end
+  }
   
   
   
 }
+
+
