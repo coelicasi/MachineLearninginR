e239c22183b15d4bab1793883731fa56a7ddf420
Alexandra McElhinney <lex.mcelhinney@gmail.com>
2015-11-19 13:27:08 +0000

    Entropy and Gain

--- README.txt
+++ README.txt
@@ -4,3 +4,19 @@ make a tree node containing that attribute
 this node partitions the data: apply the algorithm recursively.
 
 split()
+
+
+BARNOWL
+ body.length     wing.length      body.width      wing.width   
+ Min.   :2.200   Min.   :5.000   Min.   :3.000   Min.   :1.000  
+ 1st Qu.:2.625   1st Qu.:5.525   1st Qu.:4.000   1st Qu.:1.225  
+ Median :2.800   Median :5.950   Median :4.350   Median :1.300  
+ Mean   :2.785   Mean   :5.921   Mean   :4.265   Mean   :1.335  
+ 3rd Qu.:3.000   3rd Qu.:6.250   3rd Qu.:4.600   3rd Qu.:1.500  
+ Max.   :3.400   Max.   :6.900   Max.   :5.100   Max.   :1.700 
+ 
+  ent.bodylength<- -38/86*log2(38/86) - 48/86*log2(48/86)
+  for(i in barn){
+    gain<-ent.bodylength - 5/34*log2(5/34) - 29/34*log2(29/34)
+  }
+    
--- machinelearning.R
+++ machinelearning.R
@@ -8,14 +8,21 @@ owls.testing<-owls[ind==2, 1:4]#all rows with 2 are set for testing
 owls.trainLabel<-owls[ind==1, 5]
 owls.testLabel<-owls[ind==2,5]
 
-meanOwls<-by(owls[, 1:4], owls.training$type, colMeans)
+crit1<-owls.training$type=="BarnOwl"
+crit2<-owls.training$type=="LongEaredOwl"
+crit3<-owls.training$type=="SnowyOwl"
+col<-c(1:4)
+barn<-data.frame(owls.training[crit1, col])
+snowy<-data.frame(owls.training[crit2, col])
+longear<-data.frame(owls.training[crit3, col])
+barn.max<-lapply(barn, max)
+barn.min<-lapply(barn, min)
+barn.mean<-lapply(barn, mean)
 
-test<-by(owls.training, list(type=owls$type), function(x){
-    y<-subset(x, select=-type)
-    apply(y, 2, mean)
-})
-#minBarn<-apply(owls.barn, min)
-ls()#maxBarn<-apply(owls.barn, 2, max)
+a<-mean(owls.training$wing.length)
+
+totE<- -(34/86)*log2(34/86) - (28/86)*log2(28/86) - (24/86)*log2(24/86)
+
 
 #takes in the training set and outputs a prediction of trainLabel
 model<-function(owls.training, owls.trainLabel){
@@ -33,6 +40,34 @@ model<-function(owls.training, owls.trainLabel){
   }
   
   else
+    #calulate entropy. Total 86 in training. H(X) = sum((p)log2(p))
+    #body length
+  for(i in owls.training[1:4]){
     
+  }
     
+}
+
+entropy<-function(owls.training){
+  
+  count<-table(owls.training$body.length>mean(owls.training$body.length))
+  count2<-table(owls.training$body.width>mean(owls.training$body.width))
+  count3<-table(owls.training$wing.length>mean(owls.training$wing.length))
+  count4<-table(owls.training$wing.width>mean(owls.training$wing.width))
+  
+  H <- c(- count[2:2]/nrow(owls.training) * log2 (count[2:2]/nrow(owls.training))) - count[1:1]/nrow(owls.training) * log2 (count[1:1]/nrow(owls.training))
+  H2 <- c(- count2[2:2]/nrow(owls.training) * log2 (count2[2:2]/nrow(owls.training))) - count2[1:1]/nrow(owls.training) * log2 (count2[1:1]/nrow(owls.training))
+  H3 <- c(- count3[2:2]/nrow(owls.training) * log2 (count3[2:2]/nrow(owls.training))) - count3[1:1]/nrow(owls.training) * log2 (count3[1:1]/nrow(owls.training))
+  H4 <- c(- count4[2:2]/nrow(owls.training) * log2 (count4[2:2]/nrow(owls.training))) - count4[1:1]/nrow(owls.training) * log2 (count4[1:1]/nrow(owls.training))
+}
+
+gain<-function(entropy){
+  
+  df<-data.frame("body.length"=H,"body.width"= H2,"wing.length"= H3,"wing.width"= H4)
+  IG <- H - ((H2+H3+H4)/3)
+  IG2 <- H2 - ((H+H3+H4)/3)
+  IG3 <- H3 - ((H2+H+H4)/3)
+  IG4 <-H4 - ((H2+H3+H)/3)
+  df2 <- data.frame(IG, IG2, IG3, IG4)
+  
 }
