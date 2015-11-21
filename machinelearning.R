30e9312ecc5d7634278fb0b5406e376edf782d81
Alexandra McElhinney <lex.mcelhinney@gmail.com>
2015-11-21 14:38:40 +0000

    cleaning

--- machinelearning.R
+++ machinelearning.R
@@ -1,73 +1,36 @@
-owls<-read.csv("owls15.csv")
-set.seed(4)#a number of R's random number generator
-ind<-sample(2,nrow(owls), replace=TRUE, prob=c(0.67, 0.33))#sample() takes a sample with a size as big as owls
-#chooose from a vestor of 2 elements, 1 or 2, and set each row to one. 
-owls.training<-owls[ind==1, 1:5]#all rows set as 1 are for training, 2/3 of set
-#taking in random rows for testing/training. Predictions set on last columns
-owls.testing<-owls[ind==2, 1:4]#all rows with 2 are set for testing
-owls.trainLabel<-owls[ind==1, 5]
-owls.testLabel<-owls[ind==2,5]
+owls<-read.csv("owls15.csv")#read in owls data
+#creates a sample of size owls.data and sets 2/3 to 1, 1/3 to 2, randomly
+set.seed(10)
+i<- sample(2,nrow(owls), replace=TRUE, prob=c(0.67, 0.33))
+#set the training/testing
+owls.training<-owls[i==1,1:5]#includes all columns
+owls.testing<-owls[i==2, 1:4]#only includes first 4 columns
+owls.testLabels<-owls[i==2, 5]
 
-crit1<-owls.training$type=="BarnOwl"
-crit2<-owls.training$type=="LongEaredOwl"
-crit3<-owls.training$type=="SnowyOwl"
-col<-c(1:4)
-barn<-data.frame(owls.training[crit1, col])
-snowy<-data.frame(owls.training[crit2, col])
-longear<-data.frame(owls.training[crit3, col])
-barn.max<-lapply(barn, max)
-barn.min<-lapply(barn, min)
-barn.mean<-lapply(barn, mean)
+owls.pop<-nrow(owls.training)
+b<-table(owls.training$type=="BarnOwl")
+s<-table(owls.training$type=="SnowyOwl")
+l<- table(owls.training$type=="LongEaredOwl")
 
-a<-mean(owls.training$wing.length)
+ent <- - (b/owls.pop * log2(b/owls.pop))
 
-totE<- -(34/86)*log2(34/86) - (28/86)*log2(28/86) - (24/86)*log2(24/86)
+totEntropy <-  -((b[2:2]/owls.pop)*log2(b[2:2]/owls.pop) - (b[1:1]/owls.pop)*log2(b[1:1]/owls.pop))
+                 - ((s[2:2]/owls.pop)*log2(s[2:2]/owls.pop) - (s[1:1]/owls.pop)*log2(s[1:1]/owls.pop))
+                 - ((l[2:2]/owls.pop)*log2(l[2:2]/owls.pop) - (l[1:1]/owls.pop)*log2(l[1:1]/owls.pop))
 
+#takes the averages of the attributes to use in entropy calculations
+greater<-apply(owls.training[1:4], 2, function(x) length(x[mean(x)>x]))
+less<-apply(owls.training[1:4], 2, function(x) length(x[mean(x)<x]))
 
-#takes in the training set and outputs a prediction of trainLabel
-model<-function(owls.training, owls.trainLabel){
-  
-  if(length(owls.training) == 0){
-    #END 
-    print("empty training set")
-  }else if(duplicated(owls.trainLabel)){
-    print("all classes the same")
-  }
-    #end
-  else if (length(owls.trainLabel)==0){
-    print("No labels")
-    #end
-  }
-  
-  else
-    #calulate entropy. Total 86 in training. H(X) = sum((p)log2(p))
-    #body length
-  for(i in owls.training[1:4]){
-    
-  }
-    
-}
 
 entropy<-function(owls.training){
   
-  count<-table(owls.training$body.length>mean(owls.training$body.length))
-  count2<-table(owls.training$body.width>mean(owls.training$body.width))
-  count3<-table(owls.training$wing.length>mean(owls.training$wing.length))
-  count4<-table(owls.training$wing.width>mean(owls.training$wing.width))
+  for (i in owls.training[1:4]){
+    #H(s) = E p * log2 (p)
+    H <- (-less/owls.pop)* log2(less/owls.pop) - ((greater/owls.pop)*log2(greater/owls.pop))
+    gain<- totEntropy - H
+  }
   
-  H <- c(- count[2:2]/nrow(owls.training) * log2 (count[2:2]/nrow(owls.training))) - count[1:1]/nrow(owls.training) * log2 (count[1:1]/nrow(owls.training))
-  H2 <- c(- count2[2:2]/nrow(owls.training) * log2 (count2[2:2]/nrow(owls.training))) - count2[1:1]/nrow(owls.training) * log2 (count2[1:1]/nrow(owls.training))
-  H3 <- c(- count3[2:2]/nrow(owls.training) * log2 (count3[2:2]/nrow(owls.training))) - count3[1:1]/nrow(owls.training) * log2 (count3[1:1]/nrow(owls.training))
-  H4 <- c(- count4[2:2]/nrow(owls.training) * log2 (count4[2:2]/nrow(owls.training))) - count4[1:1]/nrow(owls.training) * log2 (count4[1:1]/nrow(owls.training))
-}
-
-gain<-function(entropy){
-  
-  df<-data.frame("body.length"=H,"body.width"= H2,"wing.length"= H3,"wing.width"= H4)
-  IG <- H - ((H2+H3+H4)/3)
-  IG2 <- H2 - ((H+H3+H4)/3)
-  IG3 <- H3 - ((H2+H+H4)/3)
-  IG4 <-H4 - ((H2+H3+H)/3)
-  df2 <- data.frame(IG, IG2, IG3, IG4)
+  #gain <- H[1] - (H[2]+H[3]+H[4])
   
 }
