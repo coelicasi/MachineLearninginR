416fe646049837cca5861dd606b19864baf94ea8
Alexandra McElhinney <lex.mcelhinney@gmail.com>
2015-11-25 15:07:31 +0000

    More

--- machinelearning.R
+++ machinelearning.R
@@ -1,22 +1,24 @@
 owls<-read.csv("owls15.csv")#read in owls data
 #creates a sample of size owls.data and sets 2/3 to 1, 1/3 to 2, randomly
 set.seed(10)
-i<- sample(2,nrow(owls), replace=TRUE, prob=c(0.67, 0.33))
+int<- sample(2,nrow(owls), replace=TRUE, prob=c(0.67, 0.33))
 #set the training/testing
-owls.training<-owls[i==1,1:5]#includes all columns
-owls.testing<-owls[i==2, 1:4]#only includes first 4 columns
-owls.testLabels<-owls[i==2, 5]
+owls.training<-owls[int==1,1:5]#includes all columns
+owls.testing<-owls[int==2, 1:4]#only includes first 4 columns
+owls.testLabels<-owls[int==2, 5]
+owls.trainLabels<-owls[int==1, 5]
 
-owls.pop<-nrow(owls.training)
+owls.pop<-nrow(owls.training)#total population of the training set
+
 b<-table(owls.training$type=="BarnOwl")
 s<-table(owls.training$type=="SnowyOwl")
 l<- table(owls.training$type=="LongEaredOwl")
 
-ent <- - (b/owls.pop * log2(b/owls.pop))
+#ent <- - (b/owls.pop * log2(b/owls.pop))
 
-totEntropy <-  -((b[2:2]/owls.pop)*log2(b[2:2]/owls.pop) - (b[1:1]/owls.pop)*log2(b[1:1]/owls.pop))
-                 - ((s[2:2]/owls.pop)*log2(s[2:2]/owls.pop) - (s[1:1]/owls.pop)*log2(s[1:1]/owls.pop))
-                 - ((l[2:2]/owls.pop)*log2(l[2:2]/owls.pop) - (l[1:1]/owls.pop)*log2(l[1:1]/owls.pop))
+totEntropy <-  -((b[2:2]/owls.pop)*log2(b[2:2]/owls.pop)) - ((b[1:1]/owls.pop)*log2(b[1:1]/owls.pop))
+                 - ((s[2:2]/owls.pop)*log2(s[2:2]/owls.pop)) - ((s[1:1]/owls.pop)*log2(s[1:1]/owls.pop))
+                 - ((l[2:2]/owls.pop)*log2(l[2:2]/owls.pop)) - ((l[1:1]/owls.pop)*log2(l[1:1]/owls.pop))
 
 #takes the averages of the attributes to use in entropy calculations
 greater<-apply(owls.training[1:4], 2, function(x) length(x[mean(x)>x]))
@@ -25,12 +27,46 @@ less<-apply(owls.training[1:4], 2, function(x) length(x[mean(x)<x]))
 
 entropy<-function(owls.training){
   
-  for (i in owls.training[1:4]){
-    #H(s) = E p * log2 (p)
-    H <- (-less/owls.pop)* log2(less/owls.pop) - ((greater/owls.pop)*log2(greater/owls.pop))
-    gain<- totEntropy - H
+  if(length(owls.training==0)){
+    print("Out of training data!")
+  }else if(duplicated(owls.training[, 5])){
+    print("All available classes are the same!")
+  }else{
+    for (i in owls.training[1:4]){
+      #H(s) = E p * log2 (p)
+      H <- -((less/owls.pop)* log2(less/owls.pop)) - ((greater/owls.pop)*log2(greater/owls.pop))
+      gain<- totEntropy - H
+      
+      splitter <- names(which.max(gain))
+      
+      DF <- data.frame(owls.training[1:4])
+      drops <- c(splitter)
+      newD <- DF[, !(names(DF) %in% drops)]
+      
+      for(i in newD){
+        Hnode <- -((less/owls.pop)* log2(less/owls.pop)) - ((greater/owls.pop)*log2(greater/owls.pop))
+        gainNode<- totEntropy - H
+        
+        node <- names(which.max(gain))
+        
+        DF <- data.frame(newD)
+        drops <- c(node)
+        newD2 <- DF[, !(names(DF) %in% drops)]
+      }
+      
+      root <- owls.training[splitter]
+      criteria <- sum(root)/nrow(root) #for some reason I could not perform a mean on the data
+      if(node<criteria){
+        df <- data.frame("left"==node)
+      }else{
+        df <- c(df, data.frame("right" == node))
+      }
+    }
   }
-  
-  #gain <- H[1] - (H[2]+H[3]+H[4])
-  
+ 
+
 }
+
+
+
+
